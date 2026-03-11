f66b83321c81ab97ce104288aa8ec73eaf3f55a2
Alice Frosi <afrosi@redhat.com>
2026-03-11 11:09:40 +0100

    Parametrize the images

--- install-operator.sh
+++ install-operator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Creation of the catalog for the operator
 oc apply -f catalog-source.yaml
 oc get catalogsource  -n openshift-marketplace cocl-workspace-test
@@ -12,8 +14,20 @@ oc wait --for=condition=Available deployment/confidential-cluster-operator -n co
 
 # Populate the CR
 domain=$(oc get ingress.config.openshift.io cluster -o jsonpath='{.spec.domain}{"\n"}')
-sed "s/<DOMAIN>/$domain/g" cluster-cr.yaml \
-	| oc apply -f -
+compute_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
+  jq -r '.spec.relatedImages[] | select(.name == "compute-pcrs") | .image')
+trustee_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
+  jq -r '.spec.relatedImages[] | select(.name == "trustee") | .image')
+reg_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
+  jq -r '.spec.relatedImages[] | select(.name == "registration-server") | .image')
+attest_reg_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
+  jq -r '.spec.relatedImages[] | select(.name == "attestation-key-register") | .image')
+sed "s|<DOMAIN>|$domain|g" cluster-cr.yaml | \
+	sed "s|<COMPUTE-IMG>|$compute_img|g" |\
+	sed "s|<TRUSTEE-IMG>|$trustee_img|g" |\
+	sed "s|<REG-IMG>|$reg_img|g" |\
+	sed "s|<ATTEST-REG-IMG>|$attest_reg_img|g" |\
+	oc apply -f -
 
 # Install the approved image
 oc apply -f approved-img.yaml
