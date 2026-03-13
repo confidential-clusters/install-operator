eb361a93604d47231894ce2764b68b9f5160b603
Yalan Zhang <yalzhang@redhat.com>
2026-03-13 13:50:52 +0800

    Migrate to dev-preview release and simplify operator installation
    Update the operator installation to use the official dev-preview catalog
    source from registry.redhat.io instead of the workspace test source.
    Simplify the installation process by removing manual image extraction
    from the CSV, as the operator now handles image references internally.
    
    Changes:
    - Update catalog source to use dev-preview channel and official registry
    - Remove CSV image extraction logic from install script
    - Rename resources from "cocl" to "confidential-cluster" for consistency
    - Update the READEME.md file about the CSV image extraction
    
    Signed-off-by: Yalan Zhang <yalzhang@redhat.com>

--- install-operator.sh
+++ install-operator.sh
@@ -4,7 +4,7 @@ set -e
 
 # Creation of the catalog for the operator
 oc apply -f catalog-source.yaml
-oc get catalogsource  -n openshift-marketplace cocl-workspace-test
+oc get catalogsource  -n openshift-marketplace confidential-cluster-operator-dev-preview
 
 oc apply -f namespace.yaml
 oc apply -f subscription.yaml
@@ -19,19 +19,7 @@ oc wait --for=condition=Available deployment/confidential-cluster-operator -n co
 
 # Populate the CR
 domain=$(oc get ingress.config.openshift.io cluster -o jsonpath='{.spec.domain}{"\n"}')
-compute_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
-  jq -r '.spec.relatedImages[] | select(.name == "compute-pcrs") | .image')
-trustee_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
-  jq -r '.spec.relatedImages[] | select(.name == "trustee") | .image')
-reg_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
-  jq -r '.spec.relatedImages[] | select(.name == "registration-server") | .image')
-attest_reg_img=$(oc get csv confidential-cluster-operator.v0.1.0 -n confidential-clusters -o json |\
-  jq -r '.spec.relatedImages[] | select(.name == "attestation-key-register") | .image')
 sed "s|<DOMAIN>|$domain|g" cluster-cr.yaml | \
-	sed "s|<COMPUTE-IMG>|$compute_img|g" |\
-	sed "s|<TRUSTEE-IMG>|$trustee_img|g" |\
-	sed "s|<REG-IMG>|$reg_img|g" |\
-	sed "s|<ATTEST-REG-IMG>|$attest_reg_img|g" |\
 	oc apply -f -
 
 # Install the approved image
